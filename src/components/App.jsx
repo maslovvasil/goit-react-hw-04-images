ef316cd63a2bbce9c0c5963c744a8394845a77a8
Vasyl Maslov <maslovvasil@gmail.com>
2023-09-02 14:11:44 +0300

    add App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,91 @@
+import { useState, useEffect } from 'react';
+import * as Scroll from 'react-scroll';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { BallTriangle } from 'react-loader-spinner';
+
+// import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Button from './Button';
+import getImageCollection from 'helpers/getImageCollection';
+
+import { AppContainer, LoaderContainer, NotFound } from './App.styled';
+
 const App = () => {
+  const [data, setData] = useState([]);
+  const [totalImages, setTotalImages] = useState(null);
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [panding, setPanding] = useState(false);
+  const [status, setStatus] = useState('resolves');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (search) {
+          setPanding(true);
+
+          const data = await getImageCollection(search, page);
+
+          setPanding(false);
+
+          if (data.array) {
+            if (data.array.length === 0) {
+              setStatus('rejected');
+              return;
+            }
+
+            loadMoreData(data.array);
+            setTotalImages(data.total);
+            setStatus('resolved');
+          }
+          const scroll = Scroll.animateScroll;
+          scroll.scrollToBottom();
+        }
+      } catch (error) {
+        Notify.failure(error.message);
+      }
+    };
+    fetchData();
+  }, [search, page]);
+
+  const handleSubmit = newSearch => {
+    if (search === newSearch && page === 1) {
+      Notify.failure('You allready use this request');
+      return;
+    }
+    setData([]);
+    setSearch(newSearch);
+    setPage(1);
+  };
+
+  const loadMoreData = newData => setData(data => [...data, ...newData]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <AppContainer>
+      {/* <Searchbar onSubmit={handleSubmit} /> */}
+
+      {status === 'rejected' && (
+        <NotFound>Sorry, we find nothing. Try another request</NotFound>
+      )}
+
+      {status === 'resolved' && <ImageGallery data={data} />}
+
+      {panding && (
+        <LoaderContainer>
+          <BallTriangle
+            height="80"
+            width="80"
+            color="#3f51b5"
+            ariaLabel="three-dots-loading"
+          />
+        </LoaderContainer>
+      )}
+
+      {data.length > 0 && data.length < totalImages && (
+        <Button onClick={setPage} />
+      )}
+    </AppContainer>
   );
 };
 
-export default App
+export default App;
---
+++ src/components/App.styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`;
+
+export const LoaderContainer = styled.div`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const NotFound = styled.div`
+  margin: auto;
+  font-size: 30px;
+  color: #3f51b5;
+`;
